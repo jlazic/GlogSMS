c281ec2792ac601b5915cb3fcf4de027d0038d81
Josip Lazic <josip.lazic@apprrr.hr>
2015-04-08 09:20:45 +0200

    Implement celery

--- project/__init__.py
+++ project/__init__.py
@@ -1,7 +1,6 @@
 from __future__ import absolute_import
-from django.conf import settings
 
-if settings.USE_CELERY:
-    # This will make sure the app is always imported when
-    # Django starts so that shared_task will use this app.
+try:
     from .celery import app as celery_app
+except ImportError:
+    pass
