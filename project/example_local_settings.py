9bd36402401e1c4701efe892a25cbb15cee67c56
Josip Lazic <josip@lazic.info>
2015-04-13 11:28:24 +0200

    Update settings

--- project/example_local_settings.py
+++ project/example_local_settings.py
@@ -7,15 +7,16 @@ settings = sys.modules['project.settings']
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-# Do you have celery installed? If you just want to test
-BROKER_URL = "amqp://guest:guest@localhost/sms"
-
 # Set to True if you have installed RabbitMQ, and you plan on using
 # AMQP. I highly recommend usage of AMQP. Read more in Github wiki.
 SMS_AMQP_ENABLED = True
 
 # Set according to https://pika.readthedocs.org/en/latest/examples/using_urlparameters.html
 SMS_AMQP_URL = "amqp://guest:guest@localhost/sms"
+SMS_AMQP_QUEUE = "messages"
+
+# Celery broker URL, can be the same as SMS_AMQL_URL
+BROKER_URL = "amqp://guest:guest@localhost/sms"
 
 # If true you must use passwords in phones
 SMS_USE_AUTH = True
